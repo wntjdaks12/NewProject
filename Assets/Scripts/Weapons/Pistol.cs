3e2c60b0db3bffc30012104fb6b9c7f68e5f0f9d
wntjdaks12 <qqqq8692@naver.com>
2021-12-02 03:15:54 +0900

    무기에 관련 피스톨 스크립트 추가

---
+++ Assets/Scripts/Weapons/Pistol.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Weapon
+{
+    public void Attack()
+    {
+        Debug.Log("АјАн");
+    }
+}
