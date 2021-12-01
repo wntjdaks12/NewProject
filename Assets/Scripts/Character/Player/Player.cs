7bd8ff643a10dc094a271aadff4f4fd69a9cc78c
wntjdaks12 <qqqq8692@naver.com>
2021-12-01 22:21:09 +0900

    플레이어 관련 스크립트 추가

---
+++ Assets/Scripts/Character/Player/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 플레이어입니다.
+/// </summary>
+public class Player : Character
+{
+    /// <summary>
+    /// 플레이어를 가만히 있게 합니다.
+    /// </summary>
+    public void Idle()
+    {
+        // 가만히 있는 상태로 변경합니다.
+        state.Idle(this);
+    }
+
+    /// <summary>
+    /// 플레이어를 이동시킵니다.
+    /// </summary>
+    /// <param name="pos">이동 </param>
+    /// <param name="spd">플레이어 속도</param>
+    public void Move(Vector2 rotation, float speed)
+    {
+        // 이동 상태로 변경합니다.
+        state.Move(this);
+
+        // 퍼사드 패턴
+        Direction(rotation);
+        ForwardMove(speed);
+    }
+
+    // 이동 방향을 정해줍니다.
+    private void Direction(Vector2 rotation)
+    {
+        var vec3 = new Vector3(rotation.x, rotation.y, 0) - Vector3.forward;
+        
+        transform.rotation = Quaternion.Euler(0, - Mathf.Atan2(vec3.y, vec3.x) * Mathf.Rad2Deg + 90, 0);
+    }
+    
+    // 직선 이동을 합니다.
+    private void ForwardMove(float speed)
+    {
+        if (rigid)
+            rigid.velocity =  transform.forward * speed * Time.deltaTime;
+    }
+}
