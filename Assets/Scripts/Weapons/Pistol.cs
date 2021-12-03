7b4b4222f0fdaa7a1f3c6ac27c1d626ee3ff10df
wntjdaks12 <qqqq8692@naver.com>
2021-12-03 18:48:50 +0900

    피스톨 쿨타임 추가로 수정

--- Assets/Scripts/Weapons/Pistol.cs
+++ Assets/Scripts/Weapons/Pistol.cs
@@ -2,10 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 다양한 무기중 하나인 권총입니다.
+/// </summary>
 public class Pistol : Weapon
 {
-    public void Attack()
+    // 쿨타임 컴포넌트입니다.
+    private CooldownTime cooldownTime;
+
+    public void Awake()
     {
-        Debug.Log("공격");
+        cooldownTime = GetComponent<CooldownTime>() ?? GetComponent<CooldownTime>();
+    }
+    
+    /// <summary>
+    /// 공격합니다.
+    /// </summary>
+    public override void Attack()
+    {
+        cooldownTime?.StartCooldownTime();
     }
 }
