196e6c5b7ca7afee98968bb487f91f42174cac4c
wntjdaks12 <qqqq8692@naver.com>
2021-12-04 05:32:43 +0900

    활성화/ 비활성화 부분 추가로 수정

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -7,8 +7,38 @@ using UnityEngine;
 /// </summary>
 public abstract class Weapon : MonoBehaviour
 {
+
+    // 공격 여부입니다.
+    private bool isAttacking = false;
+
     /// <summary>
     /// 공격합니다.
     /// </summary>
     public abstract void Attack();
+
+    protected void OnEnable()
+    {
+        OverlapMonsterCollider.collisionEnterEvent += ActivateAttack;
+        OverlapMonsterCollider.collisionExitEvent += DeactivateAttack;
+    }
+
+    // 공격을 활성화시킵니다.
+    private void ActivateAttack()
+    {
+        isAttacking = true;
+    }
+
+    // 공격을 비활성화시킵니다.
+    private void DeactivateAttack()
+    {
+        isAttacking = false;
+    }
+
+    private void OnDisable()
+    {
+        OverlapMonsterCollider.collisionEnterEvent -= ActivateAttack;
+        OverlapMonsterCollider.collisionExitEvent -= DeactivateAttack;
+    }
+
+    public bool IsAttacking { get { return isAttacking; } }
 }
