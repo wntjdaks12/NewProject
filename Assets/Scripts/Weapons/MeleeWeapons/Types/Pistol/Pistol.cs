53c4258772a6f6ad7240b22b2bcf1221e49b61f9
wntjdaks12 <qqqq8692@naver.com>
2021-12-06 15:56:37 +0900

    피스톨 스크립트 폴더 변경

--- Assets/Scripts/Weapons/MeleeWeapons/Types/Pistol/Pistol.cs
+++ Assets/Scripts/Weapons/MeleeWeapons/Types/Pistol/Pistol.cs
@@ -3,23 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 다양한 무기중 하나인 권총입니다.
+/// 원거리 무기 중 하나인 권총입니다.
 /// </summary>
-public class Pistol : Weapon
+public class Pistol : RangedWeapon
 {
-    // 쿨타임 컴포넌트입니다.
-    private CooldownTime cooldownTime;
-
-    private void Awake()
+    public new void Awake()
     {
-        cooldownTime = GetComponent<CooldownTime>() ?? GetComponent<CooldownTime>();
+        base.Awake();
     }
-    
+
     /// <summary>
     /// 공격합니다.
     /// </summary>
     public override void Attack()
     {
+        if (pool && cooldownTime.stateType == CooldownTime.StateType.None)
+            pool.DeQueue();
+
+        // 쿨타임을 적용합니다.
         cooldownTime?.StartCooldownTime();
     }
 }
