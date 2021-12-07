a45f4c05f34d44126c72461e4d7952a230e67766
wntjdaks12 <qqqq8692@naver.com>
2021-12-07 20:34:04 +0900

    투사체 대상 추적 기능 수정

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -2,11 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 모든 투사체의 최상위입니다.
+/// </summary>
 public class Projectile : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
+    // 물리 제어입니다.
+    private Rigidbody rigid;
+
+    /// <summary>
+    /// 투사체가 향하는 대상입니다.
+    /// </summary>
+    public GameObject target;
+
+    // 풀링할 오브젝트입니다.
+    private PoolableObject poolableObject;
+
+    private void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody>() ?? GetComponent<Rigidbody>();
+
+        poolableObject = GetComponent<PoolableObject>() ?? GetComponent<PoolableObject>();
+    }
+
+    private void FixedUpdate()
+    {
+        Trace(500);
+    }
+
+    /// <summary>
+    /// 대상을 추적하여 움직입니다.
+    /// </summary>
+    /// <param name="speed">이동 속도</param>
+    public void Trace(float speed)
+    {
+        if (!target || !rigid)
+            return;
+
+        // 대상을 바라봅니다.
+        transform.LookAt(target.transform);
+
+        // 앞으로 직진합니다.
+        rigid.velocity = transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != target.tag)
+            return;
+
+        // 충돌 대상이 추적하는 대상과 같으면 인큐를 하여 비활성화시킵니다. --------
+        target = null;
+
+        poolableObject?.EnQueue();
+        // ---------------------------------------------------------------------
     }
 }
