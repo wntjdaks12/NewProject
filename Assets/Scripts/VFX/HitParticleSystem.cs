b798825ec32e1febb7fd8cc93f039c089c3033c3
wntjdaks12 <qqqq8692@naver.com>
2021-12-08 20:45:38 +0900

    타격 제어자 스크립트 추가

---
+++ Assets/Scripts/VFX/HitParticleSystem.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 타격 이펙트를 제어자입니다.
+/// </summary>
+public class HitParticleSystem : MonoBehaviour
+{
+    /// <summary>
+    /// 해당 파티클의 지속시간입니다.
+    /// </summary>
+    public float lifeTime;
+
+    // 관계의 최상위 개체입니다.
+    private Transform parent;
+
+    // 파티클 시스템을 제어합니다.
+    private ParticleSystem ps;
+
+    private void Start()
+    {
+        parent = transform.root;
+
+        ps = GetComponent<ParticleSystem>() ?? GetComponent<ParticleSystem>();
+    }
+
+    /// <summary>
+    /// 타격 이펙트를 활성화시킵니다.
+    /// </summary>
+    /// <param name="other">충돌 대상</param>
+    public void Active(Collider other)
+    {
+        if (!ps)
+            return;
+     
+        // 충돌된 대상 중점에 타격 이펙트를 입힙니다. ----------------------
+        transform.parent = other.transform;
+        transform.localPosition = new Vector3(0, 0, 0);
+        // ----------------------------------------------------------------
+
+        // 파티클을 실행합니다.
+        ps.Play();
+
+        // 지속시간에 맞춰 파티클을 비활성화시킵니다.
+        Invoke("Deactive", lifeTime);
+    }
+
+    /// <summary>
+    /// 타격 파티클 비활성화시킵니다.
+    /// </summary>
+    public void Deactive()
+    {
+        // 본래 개체의 상속관계를 유지합니다.
+        transform.parent = parent;
+    }
+}
