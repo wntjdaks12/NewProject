b65625e4a6883566dc5c052e793a5bb533fde17d
wntjdaks12 <qqqq8692@naver.com>
2021-12-10 06:31:45 +0900

    전체적으로 수정

--- Assets/Scripts/Character/Player/PlayerController.cs
+++ Assets/Scripts/Character/Player/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// 플레이어를 제어하는 컨트롤러입니다.
+/// 플레이어의 캐릭터를 제어하는 컨트롤러입니다.
 /// </summary>
 public class PlayerController : MonoBehaviour
 {
@@ -11,4 +11,34 @@ public class PlayerController : MonoBehaviour
     /// 해당 플레이어입니다.
     /// </summary>
     public Player target;
+
+    // 플레이어 데이터 값입니다.
+    [SerializeField]
+    private PlayerData playerData;
+
+    // 조이스틱 데이터 값입니다.
+    [SerializeField]
+    private JoystickData jStickData;
+
+    private void FixedUpdate()
+    {
+        Control();
+    }
+
+    // 해당 플레이어를 제어합니다.
+    private void Control()
+    {
+        if ((!target || !jStickData) || !playerData)
+            return;
+
+        // 조이스틱 클릭 후 드래그 시 해당 플레이어는 움직입니다.
+        if (jStickData.IsTouching && jStickData.PointerPosition != Vector2.zero)
+            target.Move(jStickData.PointerPosition, playerData.Speed);
+        // 플레이어는 공격이 가능할 시 공격합니다.
+        else if (target.CheckAttack())
+            target.Attack(true);
+        // 모든 조건이 만족하지 않을 경우 가만히 있습니다.
+        else
+            target.Idle();
+    }
 }
