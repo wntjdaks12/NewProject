2f7f9fe0546844074a6a232bea5f04de6ab0f964
wntjdaks12 <qqqq8692@naver.com>
2021-12-09 23:42:02 +0900

    캐릭터 공격 상태일 때 대상 가져오는 부분 수정

--- Assets/Scripts/Character/State/Types/CharacterAttackState.cs
+++ Assets/Scripts/Character/State/Types/CharacterAttackState.cs
@@ -17,7 +17,7 @@ public class CharacterAttackState : CharacterState
     public void Attack(Character character)
     {
         // 무기 공격 범위에 접한 콜라이더들을 가져옵니다. ----------------------------
-        var colls = character.Weapon.AttackRange.OverlapBehaviour.Colliders;
+        var colls = character.Weapon.AttackRange.GetColliders();
 
         // 대상을 향해 바라봅니다. -------------------------------
         var targetPos = colls[0].transform.position;
