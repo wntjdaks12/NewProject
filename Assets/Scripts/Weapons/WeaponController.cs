8f89c2ed144c72cd58ae30a42dc0b19501900eab
wntjdaks12 <qqqq8692@naver.com>
2021-12-10 06:35:00 +0900

    모노 비헤이비어 상속,  무기 타겟 수정

--- Assets/Scripts/Weapons/WeaponController.cs
+++ Assets/Scripts/Weapons/WeaponController.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 /// <summary>
 /// 플레이어의 무기를 제어하는 컨트롤러입니다.
 /// </summary>
-public class WeaponController : PlayerController
+public class WeaponController : MonoBehaviour
 {
+    /// <summary>
+    /// 해당 플레이어입니다.
+    /// </summary>
+    public Player target;
+
     private void Start()
     {
+        if (!target)
+            return;
+            
         //임시 데이터
         var obj = Instantiate(Resources.Load("Prefabs/Weapons/Pistol"), target.transform) as GameObject;
         obj.transform.localPosition = new Vector3(0, 0, 0);
