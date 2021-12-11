5ef270ef463c688d3d738a20c11d37f42495471a
wntjdaks12 <qqqq8692@naver.com>
2021-12-12 05:02:11 +0900

    UnName1 생성 스크립트 추가

---
+++ Assets/Scripts/Generator/Character/Factory/UnName1Generator.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 몬스터(UnName1)의 생성자입니다.
+/// </summary>
+public class UnName1Generator : MonsterGenerator
+{
+    /// <summary>
+    /// 생성합니다.
+    /// </summary>
+    public override void Generate()
+    {
+        MonoBehaviour.Instantiate(Resources.Load("Prefabs/Characters/Monsters/UnName1(ObjectPooling)"));
+    }
+}
