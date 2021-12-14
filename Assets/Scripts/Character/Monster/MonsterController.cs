857c781dc206ede56f34725798916a61f2f58a49
wntjdaks12 <qqqq8692@naver.com>
2021-12-14 19:47:46 +0900

    헬스 인터페이스 추가, 널 체크 추가

--- Assets/Scripts/Character/Monster/MonsterController.cs
+++ Assets/Scripts/Character/Monster/MonsterController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 /// <summary>
 /// 몬스터를 제어하는 컨트롤러입니다.
 /// </summary>
-public class MonsterController : MonoBehaviour
+public class MonsterController : MonoBehaviour, IHealth
 {
     /// <summary>
     /// 해당 몬스터 대상입니다.
@@ -25,7 +25,8 @@ public class MonsterController : MonoBehaviour
     // 데이터를 읽습니다.
     private void DataLoad()
     {
-        data = MonsterDatabase.SearchData(target.Id);
+        if(target)
+            data = MonsterDatabase.SearchData(target.Id);
     }
 
     // 해당 플레이어를 제어합니다.
@@ -37,5 +38,15 @@ public class MonsterController : MonoBehaviour
     {
         // 해당 대상을 제어합니다.
         Control();
+    }
+
+    /// <summary>
+    /// 데미지를 입습니다.
+    /// </summary>
+    /// <param name="damage">데미지 값</param>
+    public void Damage(int damage)
+    {
+        if (data != null)
+            data.hp = HealthSystem.Damage(data.hp, data.maxHp, damage);
     }
 }
