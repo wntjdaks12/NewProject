8ee3a1f3b0b48719de011fd245fa62f62c0eb59d
wntjdaks12 <qqqq8692@naver.com>
2021-12-18 04:40:50 +0900

    캐릭터가 공격 상태로 변환 시 수행하는 부분 수정

--- Assets/Scripts/Character/State/Types/CharacterDieState.cs
+++ Assets/Scripts/Character/State/Types/CharacterDieState.cs
@@ -13,9 +13,15 @@ public class CharacterDieState : CharacterState
     /// <param name="character">캐릭터 주솟값</param>
     public void Attack(Character character)
     {
-        // 상태를 공격으로 변경합니다.
-        character.State = CharacterAttackState.Instance;
-        character.stateType = Character.StateTypes.Attack;
+        // 무기 공격 범위에 접한 콜라이더들을 가져옵니다. ----------------------------
+        var colls = character.Weapon.AttackRange.GetColliders();
+
+        // 대상을 향해 바라봅니다. -------------------------------
+        var targetPos = colls[0].transform.position;
+
+        var vec3 = targetPos - character.transform.position; vec3.y = 0;
+        character.transform.rotation = Quaternion.LookRotation(vec3);
+        // ---------------------------------------------------
     }
 
     public void Die(Character character)
