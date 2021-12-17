83b16cc5a66652e32d4a7269d72480e04f1bdad8
wntjdaks12 <qqqq8692@naver.com>
2021-12-18 04:25:11 +0900

    주석 추가

--- Assets/Scripts/Generator/GenerateStrategy/GenerateBehaviour.cs
+++ Assets/Scripts/Generator/GenerateStrategy/GenerateBehaviour.cs
@@ -9,10 +9,10 @@ using UnityEngine;
 public interface GenerateBehaviour
 {
     /// <summary>
-    /// 생성 위치를 가져옵니다.
+    /// 생성 위치를 위치 값을 계산합니다.
     /// </summary>
     /// <param name="vec3">기준 정점</param>
     /// <param name="size">기준 정점으로부터 크기</param>
-    /// <returns></returns>
+    /// <returns>생성시킬 위치를 리턴합니다.</returns>
     public Vector3 getGenerate(Vector3 vec3, float size);
 }
