75634ec7cd2df7dfa6b7665086c54e8a03a69dba
wntjdaks12 <qqqq8692@naver.com>
2021-12-20 01:56:37 +0900

    캐릭터가 죽는 상태 시 모든 상태 제거

--- Assets/Scripts/Character/State/Types/CharacterDieState.cs
+++ Assets/Scripts/Character/State/Types/CharacterDieState.cs
@@ -13,21 +13,10 @@ public class CharacterDieState : CharacterState
     /// <param name="character">캐릭터 주솟값</param>
     public void Attack(Character character)
     {
-        // 무기 공격 범위에 접한 콜라이더들을 가져옵니다. ----------------------------
-        var colls = character.Weapon.AttackRange.GetColliders();
-
-        // 대상을 향해 바라봅니다. -------------------------------
-        var targetPos = colls[0].transform.position;
-
-        var vec3 = targetPos - character.transform.position; vec3.y = 0;
-        character.transform.rotation = Quaternion.LookRotation(vec3);
-        // ---------------------------------------------------
     }
 
     public void Die(Character character)
     {
-        // 풀링 개체이므로 비활성화 시킵니다.
-        character.PoolableObject?.EnQueue();
     }
 
     /// <summary>
@@ -36,9 +25,6 @@ public class CharacterDieState : CharacterState
     /// <param name="character">캐릭터 주솟값</param>
     public void Idle(Character character)
     {
-        // 상태를 가만히 있는 것으로 변경합니다.
-        character.State = CharacterIdleState.Instance;
-        character.stateType = Character.StateTypes.Idle;
     }
 
     /// <summary>
@@ -47,9 +33,6 @@ public class CharacterDieState : CharacterState
     /// <param name="character">캐릭터 주솟값</param>
     public void Move(Character character)
     {
-        // 상태를 사망으로 변경합니다.
-        character.State = CharacterMoveState.Instance;
-        character.stateType = Character.StateTypes.Move;
     }
 
     public static CharacterDieState Instance { get { return instance; } }
