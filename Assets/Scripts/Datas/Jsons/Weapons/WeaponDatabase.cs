2ec157fb15c07b5003532ae09fd50002fe9ffeb7
wntjdaks12 <qqqq8692@naver.com>
2021-12-21 08:42:36 +0900

    데이터 경로 수정

--- Assets/Scripts/Datas/Jsons/Weapons/WeaponDatabase.cs
+++ Assets/Scripts/Datas/Jsons/Weapons/WeaponDatabase.cs
@@ -48,12 +48,11 @@ public class WeaponDatabase : MonoBehaviour
     // 데이터를 읽습니다.
     private void LoadData()
     {
-        // 해당 경로에 있는 제이슨 데이터를 체크합니다.
-        if (!new FileInfo(Application.dataPath + "/Datas/Jsons/Weapons/Weapons.json").Exists)
+        if (!new FileInfo(Application.persistentDataPath + "Weapons.json").Exists)
             return;
 
         // 해당 경로의 제이슨 데이터를 읽고 오브젝트형으로 변환합니다. ---------------------------------------
-        var dataStr = File.ReadAllText(Application.dataPath + "/Datas/Jsons/Weapons/Weapons.json");
+        var dataStr = File.ReadAllText(Application.persistentDataPath + "Weapons.json");
         datas = JsonUtility.FromJson<WeaponInfos>(dataStr);
         // ------------------------------------------------------------------------------------------------
     }
