768d16b48bad9683fd196c43bb799ac0d5e3b172
wntjdaks12 <qqqq8692@naver.com>
2021-12-21 08:43:01 +0900

    데이터 경로 수정

--- Assets/Scripts/Datas/Jsons/Characters/Monster/MonsterDatabase.cs
+++ Assets/Scripts/Datas/Jsons/Characters/Monster/MonsterDatabase.cs
@@ -20,12 +20,12 @@ public class MonsterDatabase : CharacterDatabase
     // 데이터를 읽습니다.
     private void LoadData()
     {
-        // // 해당 경로에 있는 제이슨 데이터를 체크합니다.
-        if (!new FileInfo(Application.dataPath + "/Datas/Jsons/Character/Monster/Monster.json").Exists)
+        // 해당 경로에 있는 제이슨 데이터를 체크합니다.
+        if (!new FileInfo(Application.persistentDataPath + "Monster.json").Exists)
             return;
 
         // 해당 경로의 제이슨 데이터를 읽고 오브젝트형으로 변환합니다. ---------------------------------------
-        var dataStr = File.ReadAllText(Application.dataPath + "/Datas/Jsons/Character/Monster/Monster.json");
+        var dataStr = File.ReadAllText(Application.persistentDataPath + "Monster.json");
         datas = JsonUtility.FromJson<CharacterInfos>(dataStr);
         // ------------------------------------------------------------------------------------------------
     }
@@ -37,6 +37,9 @@ public class MonsterDatabase : CharacterDatabase
     /// <returns>캐릭터 데이터를 리턴합니다.</returns>
     public static CharacterInfo SearchData(string id)
     {
+        if (datas == null)
+            return null;
+
         foreach (CharacterInfo data in datas.characterInfos)
         {
             if (id == data.id)
