6154e9833cfd3f5d4e21515ff5a8ecb2044610a2
wntjdaks12 <qqqq8692@naver.com>
2021-12-28 18:01:17 +0900

    쿨타임 mvp 모델 인터페이스 스크립트 추가

---
+++ Assets/ICooldownTimeModel.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICooldownTimeModel
+{
+    public float getCurCooldownTIme();
+    public float getCooldownTIme();
+}
