435e0b3affb6fce40d1d9ed25249d5511780cc7e
wntjdaks12 <qqqq8692@naver.com>
2021-12-29 14:20:32 +0900

    널 체크 부분 추가

--- Assets/Scripts/Weapons/RangedWeapons/RangedWeapon.cs
+++ Assets/Scripts/Weapons/RangedWeapons/RangedWeapon.cs
@@ -10,4 +10,22 @@ public abstract class RangedWeapon : Weapon
     // 투사체를 필요로 하기 때문에 오브젝트 풀링을 사용합니다.
     [SerializeField]
     protected Pool pool;
+
+    /// <summary>
+    /// 널 체크합니다.
+    /// </summary>
+    /// <returns>널이 있으면 True 없으면 False를 리턴합니다.</returns>
+    protected bool CheckEmpty()
+    {
+        if (weaponData == null || weaponData.weaponInfo == null)
+            return true;
+
+        if (CooldownTime == null || AttackRange == null)
+            return true;
+
+        if (pool != null)
+            return false;
+
+        return true;
+    }
 }
