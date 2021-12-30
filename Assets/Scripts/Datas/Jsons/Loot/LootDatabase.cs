53226d3bb4320f18f33cd86d7004d8329c89f9d6
wntjdaks12 <qqqq8692@naver.com>
2021-12-31 07:58:05 +0900

    전리품 데이터 추가

---
+++ Assets/Scripts/Datas/Jsons/Loot/LootDatabase.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+/// <summary>
+/// 전리품 데이터 정보입니다.
+/// </summary>
+[System.Serializable]
+public class LootInfo
+{
+    public string id;
+    public int probability;
+}
+
+/// <summary>
+/// 전리품 소유자 리스트입니다.
+/// </summary>
+[System.Serializable]
+public class OwnerInfo
+{
+    public string id;
+    public List<LootInfo> lootInfos;
+}
+
+/// <summary>
+/// 전리품 리스트입니다.
+/// </summary>
+[System.Serializable]
+public class LootInfos
+{
+    public List<OwnerInfo> ownerInfos;
+}
+
+/// <summary>
+/// 전리품 데이터베이스입니다.
+/// </summary>
+public class LootDatabase : MonoBehaviour
+{
+    // 전리품 데이터 리스트입니다.
+    private static LootInfos datas;
+
+    private void Start()
+    {
+        // 데이터를 읽습니다.
+        LoadData();
+    }
+
+    // 데이터를 읽습니다.
+    private void LoadData()
+    {
+        if (!new FileInfo(Application.persistentDataPath + "Loot.json").Exists)
+            return;
+
+        // 해당 경로의 제이슨 데이터를 읽고 오브젝트형으로 변환합니다. ---------------------------------------
+        var dataStr = File.ReadAllText(Application.persistentDataPath + "Loot.json");
+        datas = JsonUtility.FromJson<LootInfos>(dataStr);
+        // ------------------------------------------------------------------------------------------------
+    }
+
+    // 데이터를 찾습니다.
+    public static List<OwnerInfo> SearchData(string id)
+    {
+        if (datas == null)
+            return null;
+
+        for (int i = 0; i < datas.ownerInfos.Count; i++)
+        {
+            if (id == datas.ownerInfos[i].id)
+                return datas.ownerInfos;
+        }
+
+        return null;
+    }
+}
