3dda16f01a6dd3f31603e2172846f4ee130f7cc9
wntjdaks12 <qqqq8692@naver.com>
2021-12-30 15:30:07 +0900

    데미지 팝업 재사용 널체크 추가, 데미지 팝업 비활성화 추가

--- Assets/temp/DamagePopupView.cs
+++ Assets/temp/DamagePopupView.cs
@@ -5,16 +5,27 @@ using TMPro;
 
 public class DamagePopupView : MonoBehaviour
 {
+    // 풀링할 오브젝트입니다.
+    private PoolableObject poolableObject;
+
     private TextMeshProUGUI tmp;
 
     private void Awake()
     {
         tmp = GetComponent<TextMeshProUGUI>();
+
+        poolableObject = GetComponent<PoolableObject>();
     }
 
     public void ChangeText(string text)
     {
         if (tmp != null)
             tmp.text = text;
+    }
+
+    public void Disable()
+    {
+        if(poolableObject != null)
+            poolableObject.EnQueue();
     }
 }
