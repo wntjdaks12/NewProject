25185c5226e2a8706e8980dcc7c2bd1a6b2b1967
wntjdaks12 <qqqq8692@naver.com>
2021-12-31 19:04:35 +0900

    딕셔너리를 사용한 풀링 개체 생성

--- Assets/Scripts/Loot/LootBox.cs
+++ Assets/Scripts/Loot/LootBox.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public enum ownerIds { monster001, monster002 }
 
+/// <summary>
+/// 전리품을 생성하게 해주는 상자입니다.
+/// </summary>
 public class LootBox : MonoBehaviour
 {
     private OwnerInfo ownerInfo;
 
     public ownerIds ownerId;
 
-    private void OnEnable()
+    private Dictionary<string, Pool> poolDict = new Dictionary<string, Pool>();
+
+    private void Start()
     {
         ownerInfo = LootDatabase.SearchData(ownerId.ToString());
+
+        Init();
+    }
+
+    private void Init()
+    {
+        foreach (LootInfo data in ownerInfo.lootInfos)
+        {
+            var obj = Instantiate(Resources.Load("Loot/" + data.keyName)) as GameObject;
+
+            poolDict.Add(data.keyName, obj.GetComponent<Pool>());
+        }
     }
 
     public void Open()
@@ -22,7 +39,10 @@ public class LootBox : MonoBehaviour
             var rand = Random.Range(1, 101);
 
             if (rand <= data.probability)
-                Instantiate(Resources.Load("Loot/" + data.keyName), transform.transform.position, Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));
+            {
+                var obj =  poolDict[data.keyName].DeQueue();
+                obj.transform.position = transform.position;
+            }
         }
     }
 }
