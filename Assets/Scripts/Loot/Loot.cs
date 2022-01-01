16e9af531726044ffb0ef349ea0a4ffba813569c
wntjdaks12 <qqqq8692@naver.com>
2022-01-01 10:46:28 +0900

    주석 추가

--- Assets/Scripts/Loot/Loot.cs
+++ Assets/Scripts/Loot/Loot.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 전리품입니다.
+/// </summary>
 public class Loot : MonoBehaviour
 {
+    // 전리품을 던지기 위해 리지드바디를 가져옵니다.
     private Rigidbody rigid;
 
+    // 전리품에 대한 던지는 힘입니다.
     [SerializeField]
     private float throwingPower;
 
+    // 전리품에 대한 생존 시간입니다.
     [SerializeField]
     private float lifeTime;
 
+    // 풀링 할 오브젝트입니다.
     [SerializeField]
     private PoolableObject poolableObject;
 
@@ -20,17 +27,34 @@ public class Loot : MonoBehaviour
         rigid = GetComponent<Rigidbody>();
     }
 
-    private void Start()
+    private void OnEnable()
     {
-        if(rigid != null)
-            rigid.AddForce(new Vector3(Random.Range(-1f, 1f), Random.Range(0, 3f), Random.Range(-1f, 1f)) * throwingPower, ForceMode.Impulse);
+        Throw();
 
-        StartCoroutine(a());
+        StartCoroutine(DestroyAsync());
     }
 
-    private IEnumerator a()
+    /// <summary>
+    /// 전리품을 던집니다.
+    /// </summary>
+    private void Throw()
     {
+        // 전리품을 무작위 방향으로 던집니다. -------------------------------------------------------------------------------------------------------------------
+        if (rigid != null)
+            rigid.AddForce(new Vector3(Random.Range(-1f, 1f), Random.Range(0, 3f), Random.Range(-1f, 1f)) * throwingPower, ForceMode.Impulse);
+        // ----------------------------------------------------------------------------------------------------------------------------------------------------
+    }
+
+    /// <summary>
+    /// 전리품을 삭제시킵니다.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator DestroyAsync()
+    {
+        // 생존 시간이 달할 시 삭제시킵니다. -------------------------------
         yield return new WaitForSeconds(lifeTime);
-        poolableObject.EnQueue();
+        if(poolableObject != null)
+            poolableObject.EnQueue();
+        // ---------------------------------------------------------------
     }
 }
--- Assets/Scripts/Loot/LootBox.cs
+++ Assets/Scripts/Loot/LootBox.cs
@@ -9,10 +9,13 @@ public enum ownerIds { monster001, monster002 }
 /// </summary>
 public class LootBox : MonoBehaviour
 {
+    // 소유자 전리품 정보입니다.
     private OwnerInfo ownerInfo;
 
-    public ownerIds ownerId;
+    // 소유자 ID 값입니다.
+    private ownerIds ownerId;
 
+    // 해당 ID 값을 가진 전리품을 풀링 시키기 위한 딕셔너리입니다.
     private Dictionary<string, Pool> poolDict = new Dictionary<string, Pool>();
 
     private void Start()
@@ -22,18 +25,27 @@ public class LootBox : MonoBehaviour
         Init();
     }
 
+    /// <summary>
+    /// 초기화시킵니다.
+    /// </summary>
     private void Init()
     {
+        // 풀링할 전리품을 캐싱합니다. ----------------------------------------------------------------------
         foreach (LootInfo data in ownerInfo.lootInfos)
         {
             var obj = Instantiate(Resources.Load("Loot/" + data.keyName)) as GameObject;
 
             poolDict.Add(data.keyName, obj.GetComponent<Pool>());
         }
+        // -------------------------------------------------------------------------------------------------
     }
 
+    /// <summary>
+    /// 전리품 상자를 오픈합니다.
+    /// </summary>
     public void Open()
     {
+        // 확률에 따른 전리품을 풀링합니다. ---------------------------------
         foreach (LootInfo data in ownerInfo.lootInfos)
         {
             var rand = Random.Range(1, 101);
@@ -44,5 +56,6 @@ public class LootBox : MonoBehaviour
                 obj.transform.position = transform.position;
             }
         }
+        // -----------------------------------------------------------------
     }
 }
