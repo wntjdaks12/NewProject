bd1c7bb5ec1c2382669539edcbfda6de7a503e08
wntjdaks12 <qqqq8692@naver.com>
2022-01-03 12:08:26 +0900

    기존 쿨타임 MVP 패턴 스크립트 삭제

--- Assets/Scripts/GUI/CooldownTime/MVP/ICooldownTimeModel.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ICooldownTimeModel
-{
-    public float getCurCooldownTIme();
-    public float getCooldownTIme();
-}
