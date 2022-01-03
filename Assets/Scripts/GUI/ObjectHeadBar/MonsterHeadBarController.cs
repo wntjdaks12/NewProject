b248156d11f7c6d9de2d70cc1d0c075f78dbf6c8
wntjdaks12 <qqqq8692@naver.com>
2022-01-04 08:27:46 +0900

    UniRx 도입으로 구조 수정

--- Assets/Scripts/GUI/ObjectHeadBar/MonsterHeadBarController.cs
+++ Assets/Scripts/GUI/ObjectHeadBar/MonsterHeadBarController.cs
@@ -1,73 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx.Triggers;
+using UniRx;
 
 /// <summary>
-/// 몬스터 상단바를 제어하는 컨트롤러입니다.
+/// 몬스터 상단 바를 제어하는 컨트롤러입니다.
 /// </summary>
 public class MonsterHeadBarController : MonoBehaviour
 {
     /// <summary>
-    /// 몬스터 상단바를 제어합니다.
+    /// 상단 바 제어를 위해 가져옵니다.
     /// </summary>
-    public ObjectHeadBarView objectHeadBarView;
+    public ObjectHeadBar objectHeadBar;
 
     /// <summary>
-    /// 해당 상단바의 위치를 지정하기 가져옵니다.
+    /// 해당 상단 바의 위치를 지정하기 위해 가져옵니다.
     /// </summary>
     public Transform targetTs;
 
-    // 해당 상단바의 데이터입니다.
+    // 해당 상단 바의 데이터입니다.
     [SerializeField]
     private MonsterData monsterData;
 
     private void Start()
     {
-        if (monsterData == null)
-            return;
+        if (targetTs == null) return;
 
-        var model = new MonsterHeadBarModel();
-        model.MonsterData = monsterData;
+        if (objectHeadBar == null) return;
 
-        if (objectHeadBarView == null)
-            return;
+        // 해당 상단 바를 생성하고 UI 요소이므로 캔버스 하위로 지정합니다.
+        var obj = Instantiate(objectHeadBar.gameObject, GameObject.Find("Canvas").transform);
 
-        // 해당 컨텐츠를 생성하고 UI 요소이므로 캔버스 하위로 지정합니다.
-        objectHeadBarView = Instantiate(objectHeadBarView, GameObject.Find("Canvas").transform);
+        // 상단 바 데이터를 몬스터 데이터로 대입합니다. -----------------------------------
+        if (monsterData != null)
+        {
+            var model = new MonsterHeadBarModel();
+            model.MonsterData = monsterData;
+            obj.GetComponent<ObjectHeadBarPresenter>().Model = model;
+        }
+        // -----------------------------------------------------------------------------
 
-        objectHeadBarView.ObjectHeadBarPresenter = new ObjectHeadBarPresenter(objectHeadBarView, model);
-    }
+        objectHeadBar = obj.GetComponent<ObjectHeadBar>();
 
-    private void Update()
-    {
-        if (objectHeadBarView == null)
-            return;
-
-        // 해당 컨텐츠의 HP를 갱신합니다. -------------------------------------------------------
-        if (monsterData != null && monsterData.Data != null)
-            objectHeadBarView.ChangeHp();
-        // -------------------------------------------------------------------------------------
-    }
-
-    private void LateUpdate()
-    {
-        if (objectHeadBarView == null)
-            return;
-
-        // 해당 컨텐츠의 위치를 갱신합니다. -------------------------------------------------------------
-        if (targetTs != null)
-            objectHeadBarView.Move(Camera.main.WorldToScreenPoint(targetTs.position));
-        // ---------------------------------------------------------------------------------------------
-    }
-
-    private void OnDisable()
-    {
-        if (objectHeadBarView == null)
-            return;
-
-        // 비활성화 시 HP 체크가 안되므로 마지막으로 HP를 한 번 더 갱신합니다. --------------------------------------------------
-        if (monsterData != null && monsterData.Data != null)
-            objectHeadBarView.ChangeHp();
-        // -----------------------------------------------------------------------------------------
+        // 상단 바를 해당 타겟의 위치를 따라가게 해주는 업데이트 스트림입니다.
+        this.LateUpdateAsObservable().Subscribe(_ => objectHeadBar.Move(Camera.main.WorldToScreenPoint(targetTs.position)));
     }
 }
