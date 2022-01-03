cc7770f44ad8cecb82686bb52a8623754e373b5c
wntjdaks12 <qqqq8692@naver.com>
2022-01-04 08:22:40 +0900

    주석 추가

--- Assets/Scripts/GUI/ObjectHeadBar/MVP/IObjectHeadBarModel.cs
+++ Assets/Scripts/GUI/ObjectHeadBar/MVP/IObjectHeadBarModel.cs
@@ -7,7 +7,21 @@ using UnityEngine;
 /// </summary>
 public interface IObjectHeadBarModel
 {
+    /// <summary>
+    /// 오브젝트의 레벨입니다.
+    /// </summary>
+    /// <returns>레벨을 리턴합니다.</returns>
     public int getLv();
+
+    /// <summary>
+    /// 오브젝트의 체력입니다.
+    /// </summary>
+    /// <returns>체력을 리턴합니다.</returns>
     public int getHp();
+
+    /// <summary>
+    /// 오브젝트 최대 체력입니다.
+    /// </summary>
+    /// <returns>현재 체력을 리턴합니다.</returns>
     public int getMaaxHp();
 }
