fd09c8e00f3682036d4162a10cb672cd72a41363
wntjdaks12 <qqqq8692@naver.com>
2022-01-06 01:56:13 +0900

    람다식으로 변경

--- Assets/Scripts/AI/Object/ObjectAIDirectionBehaviour/Types/ObjectAITraceDirection.cs
+++ Assets/Scripts/AI/Object/ObjectAIDirectionBehaviour/Types/ObjectAITraceDirection.cs
@@ -10,10 +10,7 @@ public class ObjectAITraceDirection : ObjectAIDirectionBehaviour
     // 추적을 하기 위해 대상이 필요합니다.
     private GameObject target;
 
-    public ObjectAITraceDirection(GameObject target)
-    {
-        this.target = target;
-    }
+    public ObjectAITraceDirection(GameObject target) => this.target = target;
 
     /// <summary>
     /// 진행 할 방향을 가져옵니다.
@@ -22,11 +19,7 @@ public class ObjectAITraceDirection : ObjectAIDirectionBehaviour
     /// <returns>진행 방향을 리턴합니다.</returns>
     public Vector3 getDirection(Vector3 position)
     {
-        // 이동할 위치과 기준 위치을 이용하여 방향을 구합니다. ------
-        if(target)
-           return target.transform.position - position;
-        // --------------------------------------------------------
-
-        return Vector3.zero;
+        // 이동할 위치과 기준 위치을 이용하여 방향을 구합니다.
+        return target != null ? target.transform.position - position : Vector3.zero;
     }
 }
