8f80df0e9e36b1905f427ec7fd19d5b20fdfba0f
wntjdaks12 <qqqq8692@naver.com>
2022-01-06 21:11:19 +0900

    플레이어 상단 바 컨트롤러 스크립트 추가

---
+++ Assets/Scripts/GUI/ObjectHeadBar/PlayerHeadBarController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx.Triggers;
+using UniRx;
+
+public class PlayerHeadBarController : MonoBehaviour
+{
+    /// <summary>
+    /// 상단 바 제어를 위해 가져옵니다.
+    /// </summary>
+    public ObjectHeadBar objectHeadBar;
+
+    /// <summary>
+    /// 해당 상단 바의 위치를 지정하기 위해 가져옵니다.
+    /// </summary>
+    public Transform targetTs;
+
+    // 해당 상단 바의 데이터입니다.
+    [SerializeField]
+    private PlayerData playerData;
+
+    private void Start()
+    {
+        if (targetTs == null) return;
+
+        if (objectHeadBar == null) return;
+
+        if (GameObject.Find("Canvas") == null) return;
+
+        // 해당 상단 바를 생성하고 UI 요소이므로 캔버스 하위로 지정합니다.
+        var obj = Instantiate(objectHeadBar.gameObject, GameObject.Find("Canvas").transform);
+
+        // 상단 바 데이터를 몬스터 데이터로 대입합니다. -----------------------------------
+        if (playerData != null)
+        {
+            var model = new PlayerHeadBarModel();
+            model.PlayerData = playerData;
+            obj.GetComponent<ObjectHeadBarPresenter>().Model = model;
+        }
+        // -----------------------------------------------------------------------------
+
+        objectHeadBar = obj.GetComponent<ObjectHeadBar>();
+
+        // 상단 바를 해당 타겟의 위치를 따라가게 해주는 업데이트 스트림입니다.
+        this.LateUpdateAsObservable().Subscribe(_ => objectHeadBar.Move(Camera.main.WorldToScreenPoint(targetTs.position)));
+    }
+}
