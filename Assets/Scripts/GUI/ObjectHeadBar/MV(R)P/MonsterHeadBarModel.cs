f20b2af9419717299d98552a7caeb8a3ab8d524d
wntjdaks12 <qqqq8692@naver.com>
2022-01-06 21:25:54 +0900

    널 체크 -> 물음표 연산자로 수정

--- Assets/Scripts/GUI/ObjectHeadBar/MV(R)P/MonsterHeadBarModel.cs
+++ Assets/Scripts/GUI/ObjectHeadBar/MV(R)P/MonsterHeadBarModel.cs
@@ -15,32 +15,20 @@ public class MonsterHeadBarModel : IObjectHeadBarModel
     /// 오브젝트의 체력입니다.
     /// </summary>
     /// <returns>체력을 리턴합니다.</returns>
-    public int getHp()
-    {
-        if(monsterData != null && monsterData.Data != null)
-            return monsterData.Data.hp;
-        return 0;
-    }
+    public int getHp() => monsterData == null ? 0 :
+        monsterData.Data == null ? 0 : monsterData.Data.hp;
 
     /// <summary>
     /// 오브젝트의 레벨입니다.
     /// </summary>
     /// <returns>레벨을 리턴합니다.</returns>
-    public int getLv()
-    {
-        if (monsterData != null && monsterData.Data != null)
-            return monsterData.Data.lv;
-        return 0;
-    }
+    public int getLv() => monsterData == null ? 0 :
+        monsterData.Data == null ? 0 : monsterData.Data.lv;
 
     /// <summary>
     /// 오브젝트 최대 체력입니다.
     /// </summary>
     /// <returns>현재 체력을 리턴합니다.</returns>
-    public int getMaaxHp()
-    {
-        if (monsterData != null && monsterData.Data != null)
-            return monsterData.Data.maxHp;
-        return 0;
-    }
+    public int getMaaxHp() => monsterData == null ? 0 :
+        monsterData.Data == null ? 0 : monsterData.Data.maxHp;
 }
