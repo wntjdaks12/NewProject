0f738ffc0696bed16aa26b2374c77462dc27e887
wntjdaks12 <qqqq8692@naver.com>
2022-01-09 06:16:17 +0900

    대거 수정

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -11,39 +11,23 @@ public class Projectile : MonoBehaviour
     // 물리 제어입니다.
     private Rigidbody rigid;
 
-    /// <summary>
-    /// 투사체가 향하는 대상입니다.
-    /// </summary>
-    public GameObject target;
-    /// <summary>
-    /// 투사체를 사용한 소유자입니다.
-    /// </summary>
-    private GameObject owner;
+    // 투사체를 사용한 소유자와 향하는 대상입니다.
+    private GameObject owner, target;
 
     // 풀링할 오브젝트입니다.
     private PoolableObject poolableObject;
 
-    /// <summary>
-    /// 충돌 시 이벤트입니다.
-    /// </summary>
+    // 충돌 시 이벤트입니다.
     public UnityEvent<Collider> triggerEvent;
+
+    // 투사체의 공격 행위자입니다.
+    protected ProjectileAttackBehaviour projectileBehaviour;
 
     private void Awake()
     {
-        rigid = GetComponent<Rigidbody>() ?? GetComponent<Rigidbody>();
+        rigid = GetComponent<Rigidbody>();
 
-        poolableObject = GetComponent<PoolableObject>() ?? GetComponent<PoolableObject>();
-    }
-
-    private void FixedUpdate()
-    {
-        Trace(1500);
-
-        // 대상한테 접근할 시 비활성화 상태이면 스스로 없어집니다. ------------------------------
-        if ((target.transform.position - transform.position).sqrMagnitude < 0.1f)
-            if(!target.activeSelf)
-                Destroy();
-        // -----------------------------------------------------------------------------------
+        poolableObject = GetComponent<PoolableObject>();
     }
 
     /// <summary>
@@ -52,21 +36,28 @@ public class Projectile : MonoBehaviour
     /// <param name="speed">이동 속도</param>
     public void Trace(float speed)
     {
-        if (!target || !rigid)
-            return;
-        
-        // 대상을 바라봅니다.
-        transform.LookAt(target.transform);
+        if (!target || !rigid) return;
 
+        if ((target.transform.position - transform.position).sqrMagnitude > 0.1f)
+        {
+            transform.LookAt(target.transform);
+
+            Move(speed);
+        }
+    }
+
+    public void Move(float speed)
+    {
         // 앞으로 직진합니다.
         rigid.velocity = transform.forward * speed * Time.deltaTime;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        // 충돌 대상이 추적하고 있는 대상인지 판단합니다.
-        if (other.tag != target.tag)
-            return;
+        if (target != null)
+            // 충돌 대상이 추적하고 있는 대상인지 판단합니다.
+            if (other.tag != target.tag)
+                return;
 
         // 대상에게 데미지를 입힙니다.
         Hit(other);
@@ -75,15 +66,14 @@ public class Projectile : MonoBehaviour
         triggerEvent?.Invoke(other);
 
         // 삭제합니다. (풀링 비활성화)
-        Destroy();
+        // Destroy();
+        target = null;
     }
 
     // 삭제합니다. (풀링 비활성화)
-    private void Destroy()
+    public void Destroy()
     {
         // 풀링 개체이므로 비활성화시킵니다. ---------------------------------------------------
-        target = null;
-
         poolableObject?.EnQueue();
         // ---------------------------------------------------------------------
     }
@@ -98,5 +88,10 @@ public class Projectile : MonoBehaviour
     /// <summary>
     /// 투사체를 사용한 소유자입니다.
     /// </summary>
-    public GameObject Owner { set { owner = value; } }
+    public GameObject Owner { set => owner = value; }
+
+    /// <summary>
+    /// 투사체가 향하는 대상입니다.
+    /// </summary>
+    public GameObject Target { get => target; set => target = value; }
 }
