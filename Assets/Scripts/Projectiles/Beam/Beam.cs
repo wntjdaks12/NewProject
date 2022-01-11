a8c842b46de110073ffedf3deb0eca7475073985
wntjdaks12 <qqqq8692@naver.com>
2022-01-11 23:30:39 +0900

    주석 추가

--- Assets/Scripts/Projectiles/Beam/Beam.cs
+++ Assets/Scripts/Projectiles/Beam/Beam.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 using UniRx;
 using UniRx.Triggers;
 
+/// <summary>
+/// 투사체중 하나인 빔입니다.
+/// </summary>
 public class Beam : Projectile
 {
     private void Start()
     {
+        // 해당 투사체는 관통 공격을 합니다. ------------------------------------------------------------
         projectileAttackBehaviour = this.gameObject.AddComponent<ProjectilePiercingAttack>();
 
         projectileAttackBehaviour.Projectile = this;
+        // --------------------------------------------------------------------------------------------
 
         // 타겟을 추적하는 스트림입니다.
         this.FixedUpdateAsObservable()
