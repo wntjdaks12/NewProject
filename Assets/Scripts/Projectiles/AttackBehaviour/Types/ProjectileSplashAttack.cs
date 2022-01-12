c2887645ebfb821510d723139f5c2d1e664d4c00
wntjdaks12 <qqqq8692@naver.com>
2022-01-12 20:41:49 +0900

    투사체 스플래시 공격 추가

---
+++ Assets/Scripts/Projectiles/AttackBehaviour/Types/ProjectileSplashAttack.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+/// <summary>
+/// 투사체의 스플래시 공격입니다.
+/// </summary>
+public class ProjectileSplashAttack : ProjectileAttackBehaviour
+{
+    private void Start()
+    {
+        // 타겟이 없을 경우 비활성화시키는 스트림입니다.
+        this.UpdateAsObservable()
+            .Where(_ => projectile != null)
+            .Where(_ => projectile.Target == null)
+            .Subscribe(_ => projectile.Disable());
+
+        // 충돌 효과를 보여주는 스트림입니다.
+        this.OnTriggerEnterAsObservable()
+            .Where(_ => projectile != null)
+            .Where(other => (projectile.Target != null && other.tag == projectile.Target.tag))
+            .Subscribe(other => CheckSplash());
+    }
+
+    /// <summary>
+    /// 스플래시를 체크합니다.
+    /// </summary>
+    private void CheckSplash()
+    {
+        // 해당 반경 안의 충돌체를 검사합니다.
+        Collider[] colls = Physics.OverlapSphere(transform.position, 1, 1 << 6);
+
+        foreach (Collider coll in colls)
+        {
+            projectile.TriggerEventHandler(coll.gameObject);
+        }
+    }
+}
