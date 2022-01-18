03a0eeccfb999c2bda8f8bb6d6bcdca41eeca6cc
wntjdaks12 <qqqq8692@naver.com>
2022-01-18 23:59:41 +0900

    스테이지안의 스테이지 데이터 추가

--- Assets/Scripts/Datas/Jsons/Stages/StageDataBase.cs
+++ Assets/Scripts/Datas/Jsons/Stages/StageDataBase.cs
@@ -3,24 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-
-public class StageInfo
+[System.Serializable]
+public class Stage2Info
 {
     [HideInInspector]
     public string id;
     [HideInInspector]
     public string image;
+    [HideInInspector]
+    public string keyName;
 
-    public StageInfo(string id, string image)
+    public Stage2Info(string id, string image, string keyName)
     {
         this.id = id;
         this.image = image;
+        this.keyName = keyName;
+    }
+}
+
+[System.Serializable]
+public class StageInfo
+{
+    [HideInInspector]
+    public string id;
+    public List<Stage2Info> stage2Info = new List<Stage2Info>();
+
+    public StageInfo(string id, List<Stage2Info> stage2Info)
+    {
+        this.id = id;
+        this.stage2Info = stage2Info;   
     }
 }
 
 public class StageInfos
 {
-    public List<StageInfo> stageInfos;
+    public List<StageInfo> stageInfos = new List<StageInfo>();
 }
 
 public class StageDataBase : MonoBehaviour
@@ -32,7 +49,15 @@ public class StageDataBase : MonoBehaviour
     private void LoadData()
     {
         if (!new FileInfo(Application.persistentDataPath + "Stages.json").Exists)
+        {
+          //  datas = new StageInfos();
+          //  datas.stageInfos = new List<StageInfo>();
+          //  var a = new List<Stage2Info>();
+          //  a.Add(new Stage2Info("stage1_1_001", "iamge"));
+          //  datas.stageInfos.Add(new StageInfo("stage1_001", a));
+           // File.WriteAllText(Application.persistentDataPath + "Stages.json", JsonUtility.ToJson(datas));
             return;
+        }
 
         // 해당 경로의 제이슨 데이터를 읽고 오브젝트형으로 변환합니다. ---------------------------------------
         var dataStr = File.ReadAllText(Application.persistentDataPath + "Stages.json");
@@ -42,8 +67,7 @@ public class StageDataBase : MonoBehaviour
 
     public static StageInfo SearchData(string id)
     {
-        if (datas == null)
-            return null;
+        if (datas == null) return null;
 
         foreach (StageInfo data in datas.stageInfos)
         {
@@ -52,15 +76,5 @@ public class StageDataBase : MonoBehaviour
         }
 
         return null;
-    }
-
-    public static StageInfo DeepCopy(StageInfo data)
-    {
-        StageInfo stageInfo = new StageInfo(
-            data.id,
-            data.image
-        );
-
-        return stageInfo;
     }
 }
