0be437ad04fc161a65f4f7f4e2ee942b4249fec4
wntjdaks12 <qqqq8692@naver.com>
2022-01-24 23:47:56 +0900

    무기 정보 데이터 초기화 추가

--- Assets/Scripts/GUI/BasicAttackButton/MV(R)P/BasicAttackButtonModel.cs
+++ Assets/Scripts/GUI/BasicAttackButton/MV(R)P/BasicAttackButtonModel.cs
@@ -11,7 +11,11 @@ public class BasicAttackButtonModel
     // 무기 데이터입니다.
     private WeaponData weaponData;
 
-    public BasicAttackButtonModel() => weaponData = (WeaponData)Resources.Load("Datas/ScriptableObjects/Weapon/Weapon Data");
+    public BasicAttackButtonModel()
+    {
+        weaponData = (WeaponData)Resources.Load("Datas/ScriptableObjects/Weapon/Weapon Data");
+        weaponData.weaponInfo = new WeaponInfo(null, null, "BasicAttack_Null", null, 0, 0);
+    }
 
     // 무기의 기본 공격 이미지를 리턴합니다.
     public string BasicAttackImage { get => weaponData.weaponInfo.basicAttackImg; }
