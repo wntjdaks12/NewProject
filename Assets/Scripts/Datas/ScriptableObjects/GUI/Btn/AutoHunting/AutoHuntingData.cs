8655efdf55c6ba279dbd0271f50d9b6695c75652
wntjdaks12 <qqqq8692@naver.com>
2022-01-24 14:49:21 +0900

    자동 사냥 기능 삭제

--- Assets/Scripts/Datas/ScriptableObjects/GUI/Btn/AutoHunting/AutoHuntingData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "AutoHunting Data")]
-public class AutoHuntingData : ScriptableObject
-{
-    public bool isAuto;
-}
