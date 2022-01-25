b6cfb9f26db595543b1e8b09291dfe71544d9a55
wntjdaks12 <qqqq8692@naver.com>
2022-01-25 22:23:58 +0900

    스크립터블 데이터 삭제

--- Assets/Scripts/Weapons/WeaponController.cs
+++ Assets/Scripts/Weapons/WeaponController.cs
@@ -1,16 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UniRx;
 
 /// <summary>
 /// 플레이어의 무기를 제어하는 컨트롤러입니다.
 /// </summary>
 public class WeaponController : MonoBehaviour
 {
-    //무기 데이터입니다.
-    [SerializeField]
-    private WeaponData weaponData;
+    private WeaponInfo data;
 
     private GameObject weaponObj;
 
@@ -22,12 +19,7 @@ public class WeaponController : MonoBehaviour
     /// </summary>
     public Player target;
 
-    private void Start()
-    {
-        // 무기 데이터의 ID값이 변할 경우 해당 무기를 장착하는 스트림입니다. 
-        weaponData.ObserveEveryValueChanged(_ => weaponData.weaponInfo.id)
-            .Subscribe(_ => EquipWeapon());
-    }
+    private void Start() => EquipWeapon();
 
     /// <summary>
     /// 무기를 장착합니다.
@@ -41,21 +33,23 @@ public class WeaponController : MonoBehaviour
 
         if (data == null) return;
 
-        weaponData.weaponInfo = data;
+        this.data = data;
         // ----------------------------------------------------------------
 
         // 해당 무기를 생성하고 위치를 초기화시킵니다. -----------------------------------------------------------------------------------------
-        weaponObj = Instantiate(Resources.Load("Prefabs/Weapons/" + weaponData.weaponInfo.keyName), target.transform) as GameObject;
+        weaponObj = Instantiate(Resources.Load("Prefabs/Weapons/" + this.data.keyName), target.transform) as GameObject;
         weaponObj.transform.localPosition = new Vector3(0, 0, 0);
         // -----------------------------------------------------------------------------------------------------------------------------------
 
         // 무기 데이터 정보를 넘겨주고 대상의 무기를 교체합니다. -------------
         var weapon = weaponObj.GetComponent<Weapon>();
 
-        weapon.WeaponData = weaponData;
+      //  weapon.WeaponData = weaponData;
         target.Weapon = weapon;
 
         if(weapon.CooldownTime != null) cooldownTimeData.CooldownTimeInfo = weapon.CooldownTime.CooldownTimeInfo;
         // ---------------------------------------------------------------
     }
+
+    public WeaponInfo Data { get => data; }
 }
