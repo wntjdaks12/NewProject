97aae0ef31779f6e6971a3641f9a43841288f46f
wntjdaks12 <qqqq8692@naver.com>
2022-01-25 16:08:31 +0900

    대거 수정

--- Assets/Scripts/Cameras/MainCameraController.cs
+++ Assets/Scripts/Cameras/MainCameraController.cs
@@ -1,36 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
 
-/// <summary>
-/// 메인 카메라를 제어하는 컨트롤러입니다.
-/// </summary>
 public class MainCameraController : MonoBehaviour
 {
-    /// <summary>
-    /// 기준이 되는 대상입니다.
-    /// </summary>
-    public GameObject standardObject;
+    public DragPnlData data;
 
-    // 기준 대상과의 거리를 유지하는 정점입니다
-    [SerializeField]
-    private Vector3 point;
-
-    public void LateUpdate()
+    private void Start()
     {
-        // 이동합니다.
-        Move();
+        this.LateUpdateAsObservable()
+            .Subscribe(_ => OnDrag());
     }
 
-    // 이동합니다.
-    private void Move()
+    private void OnDrag()
     {
-        // 널 체크 ------------------------------------------
-        if (!standardObject)
-            return;
-        // -------------------------------------------------
-
-        // 대상의 위치와 기준점을 이용하여 대상과의 거리를 유지합니다.
-        transform.position = standardObject.transform.position + point;
+        var val = data.dragPosition / data.speed;
+        transform.position = new Vector3(val.x, transform.position.y, val.y);
     }
 }
