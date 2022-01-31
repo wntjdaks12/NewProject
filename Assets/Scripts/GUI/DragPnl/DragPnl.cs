6c857d52151e317cd3f3d9d43bd17e8155ea96e1
wntjdaks12 <qqqq8692@naver.com>
2022-02-01 01:48:18 +0900

    주석 추가, 변수명 변경

--- Assets/Scripts/GUI/DragPnl/DragPnl.cs
+++ Assets/Scripts/GUI/DragPnl/DragPnl.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+// 화면을 드래그할 패널입니다.
 public class DragPnl : MonoBehaviour, IPointerDownHandler, IDragHandler
 {
     public DragPnlData data;
 
-    private Vector2 startPos;
+    // 처음으로 터치한 포지션입니다.
+    private Vector2 startTouchPos;
 
-    private void Awake() => data.dragPosition = Vector2.zero;
+    private void Awake() => data.dragValue = Vector2.zero;
 
     public void OnDrag(PointerEventData eventData)
     {
-        var pos = data.dragPosition = startPos - eventData.position;
-        data.dragPosition = new Vector2(Mathf.Clamp(pos.x, data.dragMin.x, data.dragMax.x), Mathf.Clamp(pos.y, data.dragMin.y, data.dragMax.y));
+        // 최솟값, 최댓값에 따른 드래그할 값을 저장합니다.
+        var pos = data.dragValue = startTouchPos - eventData.position;
+        data.dragValue = new Vector2(Mathf.Clamp(pos.x, data.dragMin.x, data.dragMax.x), Mathf.Clamp(pos.y, data.dragMin.y, data.dragMax.y));
     }
 
-    public void OnPointerDown(PointerEventData eventData) => startPos = eventData.position + data.dragPosition;
+    // 처음으로 터치 시 포지션을 저장합니다.
+    public void OnPointerDown(PointerEventData eventData) => startTouchPos = eventData.position + data.dragValue;
 }
