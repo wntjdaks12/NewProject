b4306bc045e3196d6a8daf8fdb1c7788fd5b4062
wntjdaks12 <qqqq8692@naver.com>
2022-02-02 01:53:34 +0900

    클래스명 챔피언으로 변경

--- Assets/Scripts/Character/Player/Types/CannonShooterChampion.cs
+++ Assets/Scripts/Character/Player/Types/CannonShooterChampion.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CannonShooterChampion : Player
+public class CannonShooterChampion : Champion
 {
     private new void Awake()
     {
