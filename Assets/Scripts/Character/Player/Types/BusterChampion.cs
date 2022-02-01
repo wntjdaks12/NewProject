cca1e45e177e919241d6756ae451f26ff82a0f84
wntjdaks12 <qqqq8692@naver.com>
2022-02-02 01:53:19 +0900

    클래스명 챔피언으로 변경

--- Assets/Scripts/Character/Player/Types/BusterChampion.cs
+++ Assets/Scripts/Character/Player/Types/BusterChampion.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BusterChampion : Player
+public class BusterChampion : Champion
 {
     private new void Awake()
     {
