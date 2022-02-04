3dc62ea0f64b1a28fd6bbd489a6b89df442fb5e9
wntjdaks12 <qqqq8692@naver.com>
2022-02-05 01:58:40 +0900

    주석 변경, 파라미터명 변경

--- Assets/Scripts/Generator/GenerateStrategy/GenerateBehaviour.cs
+++ Assets/Scripts/Generator/GenerateStrategy/GenerateBehaviour.cs
@@ -8,11 +8,10 @@ using UnityEngine;
 /// 
 public interface GenerateBehaviour
 {
-    /// <summary>
     /// 생성 위치를 위치 값을 계산합니다.
     /// </summary>
-    /// <param name="vec3">기준 정점</param>
-    /// <param name="size">기준 정점으로부터 크기</param>
-    /// <returns>생성시킬 위치를 리턴합니다.</returns>
-    public Vector3 getGenerate(Vector3 vec3, float size);
+    /// <param name="pos">기준점</param>
+    /// <param name="radius">반경</param>
+    /// <returns></returns>
+    public Vector3 getGenerate(Vector3 pos, float radius);
 }
