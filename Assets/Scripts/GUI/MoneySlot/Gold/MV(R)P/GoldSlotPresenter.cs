6cde3d993c2c03208c6712969524197e765e2c17
wntjdaks12 <qqqq8692@naver.com>
2022-02-14 02:37:19 +0900

    tmp 제거, 돈 슬롯 상속, 공통 부분 제거

--- Assets/Scripts/GUI/MoneySlot/Gold/MV(R)P/GoldSlotPresenter.cs
+++ Assets/Scripts/GUI/MoneySlot/Gold/MV(R)P/GoldSlotPresenter.cs
@@ -2,25 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
-using TMPro;
 
 /// <summary>
 /// 골드 슬롯의 UGUI와 데이터를 연결해 주는 매개자입니다. 
 /// </summary>
-public class GoldSlotPresenter : MonoBehaviour
+public class GoldSlotPresenter : MoneySlotPresenter
 {
-    // 돈 데이터입니다.
-    [SerializeField]
-    private MoneySlotModel model;
-
-    // 골드 슬롯의 데이터입니다.
-    [SerializeField]
-    private TextMeshProUGUI goldTmp;
-
     private void Start()
     {
-        if(goldTmp != null)
-            model.ObserveEveryValueChanged(model => model.Data.gold)
-                .Subscribe(gold => goldTmp.text = gold.ToString());
+        if (model != null && moneyTmp != null)
+            model.ObserveEveryValueChanged(_ => model.Gold)
+                .Subscribe(gold => moneyTmp.text = gold.ToString());
     }
 }
