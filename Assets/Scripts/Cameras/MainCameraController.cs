f9429c4081fe8bf83b7889d4637ad7f53a71003a
wntjdaks12 <qqqq8692@naver.com>
2022-02-20 23:11:49 +0900

    카메라 이동시 z값 고정

--- Assets/Scripts/Cameras/MainCameraController.cs
+++ Assets/Scripts/Cameras/MainCameraController.cs
@@ -13,6 +13,6 @@ public class MainCameraController : MonoBehaviour
         // 카메라 이동 스트림입니다.
         this.LateUpdateAsObservable()
             .Select(_ => data.dragValue / data.speed)
-            .Subscribe(val => transform.position = new Vector3(val.x, transform.position.y, val.y));
+            .Subscribe(val => transform.position = new Vector3(val.x, transform.position.y, transform.position.z));
     }
 }
