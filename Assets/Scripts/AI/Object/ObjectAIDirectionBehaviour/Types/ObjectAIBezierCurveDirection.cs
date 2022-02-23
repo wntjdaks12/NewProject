d638a600f40068ece98e5f67e9c5dff70f557a81
wntjdaks12 <qqqq8692@naver.com>
2022-02-24 07:43:05 +0900

    코드 주석 처리

--- Assets/Scripts/AI/Object/ObjectAIDirectionBehaviour/Types/ObjectAIBezierCurveDirection.cs
+++ Assets/Scripts/AI/Object/ObjectAIDirectionBehaviour/Types/ObjectAIBezierCurveDirection.cs
@@ -6,25 +6,30 @@ using UnityEngine;
 /// 베지어 곡선을 이용한 인공 지능 방향 행위자입니다.
 /// </summary>
 public class ObjectAIBezierCurveDirection : ObjectAIDirectionBehaviour
-{ 
-    // 베지어 곡선의 정보를 알기 위해 인공 지능을 가져옵니다.
-    private ObjectAI objectAI;
+{
+    /*  // 베지어 곡선의 정보를 알기 위해 인공 지능을 가져옵니다.
+      private ObjectAI objectAI;
 
-    public ObjectAIBezierCurveDirection(ObjectAI objectAI) => this.objectAI = objectAI;
+      public ObjectAIBezierCurveDirection(ObjectAI objectAI) => this.objectAI = objectAI;
 
-    /// <summary>
-    /// 진행 할 방향을 가져옵니다.
-    /// </summary>
-    /// <param name="position">기준 위치 값</param>
-    /// <returns>진행 방향을 리턴합니다.</returns>
+      /// <summary>
+      /// 진행 할 방향을 가져옵니다.
+      /// </summary>
+      /// <param name="position">기준 위치 값</param>
+      /// <returns>진행 방향을 리턴합니다.</returns>
+      public Vector3 getDirection(Vector3 position)
+      {
+          // 이동할 위치와 기준 위치을 이용하여 방향을 구합니다. -------------------------
+          if (objectAI)
+              if (objectAI.Points != null)
+                  return objectAI.Points[objectAI.CurPartIndex] - position;
+          // --------------------------------------------------------------------------
+
+          return Vector3.zero;
+      }*/
+
     public Vector3 getDirection(Vector3 position)
     {
-        // 이동할 위치와 기준 위치을 이용하여 방향을 구합니다. -------------------------
-        if (objectAI)
-            if (objectAI.Points != null)
-                return objectAI.Points[objectAI.CurPartIndex] - position;
-        // --------------------------------------------------------------------------
-
         return Vector3.zero;
     }
 }
